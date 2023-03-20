0c7719589b30e1a66ecad9dcfa1d5e142d84aec8
Matheus-Vicente <mv_vieira96@hotmail.com>
2023-03-20 02:04:15 -0300

    excluindo imports não usados auth.guard

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
 
 import { AuthService } from './../services/auth.service';
 
@@ -13,7 +12,7 @@ export class AuthGuard implements CanActivate {
 
   canActivate(): boolean {
     this.authService.updateLoggedIn();
-    
+
     if (localStorage.getItem('token')) {
       return true;
     } else {
